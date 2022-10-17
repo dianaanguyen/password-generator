4c084b9a4bc2801f8d580d4c32a34f5585819916
Diana Nguyen <diananguyen@Dianas-MBP-2.attlocal.net>
2022-10-16 17:12:07 -0700

    added prompts to generate pw

--- script.js
+++ script.js
@@ -18,3 +18,15 @@ var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var lowercase = uppercase.map((letter) => letter.toLowerCase());
 var numbers = [0 , 1, 2, 3, 4, 5, 6, 7, 8, 9]
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(",")", "-", "_", "+", "=", "[","]", "{","}", "|", ",", ";", ":", "~"];
+
+//Function to generate password
+function generatePassword () {
+  var passwordLength = prompt ("How many characters do you want in the password? (Between 8-128 characters)");
+  var useUpperCase = prompt ("Would you like to include uppercase letters? Type 1 for yes and 0 for no.");
+  var useLowercase = prompt ("Would you like to include lowercase letters? Type 1 for yes and 0 for no.");
+  var useNumbers = prompt ("Would you like to include numbers? Type 1 for yes and 0 for no.");
+  var useSpecialChars = prompt ("Would you like to include special characters? Type 1 for yes and 0 for no.")
+} 
+if (passwordLength <= 7 || passwordLength >= 129 ){
+    alert ("You did not type in s number between 8 and 128, please choose again. ")
+  }
