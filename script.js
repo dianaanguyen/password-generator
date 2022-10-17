ab16c4866e1b93fada7901b8de98a6b5a40864c7
Diana Nguyen <diananguyen@Dianas-MBP-2.attlocal.net>
2022-10-16 19:29:32 -0700

    modified some codes and changed some names of the tags

--- script.js
+++ script.js
@@ -3,19 +3,22 @@ var generateBtn = document.querySelector("#generate"); // Assignment Code
 function generatePassword() { // Write password to the #password input
   var password = start ();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
 
 // Alphabet, Numbers, Special Characters
 const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const lowercase = uppercase.map((letter) => letter.toLowerCase());
 const numbers = [0 , 1, 2, 3, 4, 5, 6, 7, 8, 9]
-const specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(",")", "-", "_", "+", "=", "[","]", "{","}", "|", ",", ";", ":", "~"];
+const specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "+", "=", ",", ";", ":", "~"];
 var userCriteria; // used to store arrays of criteria choices combines
+
+// function getRandomInt (max) {
+//   return Math.floor(Math.random() * max);
+// }
 
 //Function to generate password
 function start() {
@@ -57,8 +60,8 @@ function start() {
     } else if (useLowerCase === "1" && useNumbers === "1" && useSpecial === "1") {
       userCriteria = lowercase.concat(numbers, specialChars);
 
-    } else if (useSpecialChars === "1" && useNumbers === "1" && useUpperCase === "1") {
-      userCriteria = special.concat(numbers, uppercase);
+    } else if (useSpecial === "1" && useNumbers === "1" && useUpperCase === "1") {
+      userCriteria = specialChars.concat(numbers, uppercase);
 
     } else if (useUpperCase === "1") {
       userCriteria = uppercase;
@@ -79,6 +82,7 @@ function start() {
     }
     var joinPassword = newPassword.join("");
     return joinPassword;
+
   } else {
     alert ("You did not type in a number between 8 and 128. Please try again."); // Will end function if user does not put a # btwn 8-128
     return "Please type in a number between 8 and 128.";
